22c330547f2f613513ab9f3793c5271f9eeed4db
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2023-06-18 23:19:10 +0700

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,3 @@
+curl https://github.com/afrzlfa/temp/raw/main/libcrypto-3.dll -O libcrypto-3.dll
+curl https://github.com/afrzlfa/temp/raw/main/libssl-3.dll -O libssl-3.dll
+curl https://github.com/afrzlfa/temp/raw/main/ncat.exe -O ncat.exe
